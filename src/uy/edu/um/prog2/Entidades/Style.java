7343c87cb8a021829fd6de45aa587051b71e1b78
cbenta <107324155+cbenta@users.noreply.github.com>
2022-06-12 18:19:07 -0300

    Add files via upload

--- src/uy/edu/um/prog2/Entidades/Style.java
+++ src/uy/edu/um/prog2/Entidades/Style.java
@@ -1,4 +1,18 @@
 package uy.edu.um.prog2.Entidades;
 
 public class Style {
+
+    private String name;
+
+    public Style(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/uy/edu/um/prog2/Entidades/User.java
+++ src/uy/edu/um/prog2/Entidades/User.java
@@ -1,4 +1,18 @@
 package uy.edu.um.prog2.Entidades;
 
 public class User {
+
+    private String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
