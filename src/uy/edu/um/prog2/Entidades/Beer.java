7055b1d36c4a712a38581ceff59b5b8478258e97
cbenta <107324155+cbenta@users.noreply.github.com>
2022-06-13 16:33:33 -0300

    Add files via upload

--- src/uy/edu/um/prog2/Entidades/Beer.java
+++ src/uy/edu/um/prog2/Entidades/Beer.java
@@ -5,11 +5,21 @@ public class Beer {
     private long id;
     private String name;
     private double abv;
+    private Style style;
 
     public Beer(long id, String name, double abv) {
         this.id = id;
         this.name = name;
         this.abv = abv;
+        // this.style = new Style(); // FIXME para definir que el atributo style sea una instancia de la clase Style es necesario pasarle como atributo el nombre del style.
+    }
+
+    public Style getStyle() {
+        return style;
+    }
+
+    public void setStyle(Style style) {
+        this.style = style;
     }
 
     public long getId() {
